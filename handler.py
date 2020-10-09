c60ade4cf206fc78d0b9ed9d9ac8a14f15ada2dd
Sakshi9111 <45375857+Sakshi9111@users.noreply.github.com>
2020-10-09 14:45:52 +0545

    Update handler.py

--- handler.py
+++ handler.py
@@ -4,22 +4,38 @@ import json
 api_key = "0d85e73c488e8b4db1fc5a14cd04dc27"
 base_url = "http://api.openweathermap.org/data/2.5/weather?"
 
-def weatherr(event, context):
-    city_name = event["currentIntent"]["slots"]["weather"]
+
+def get_weather(event, context):
+    """
+        Takes the currentinternt and the context value from
+        aws lex.
+        This handler only works after the slots are filled.
+        This handler response back to aws lex at Fulfilled state only.
+        It doesn't do any initialization and validation works. 
+        Make sure you untick intialization and validation option in aws lex.
+    """
+    city_name = event["currentIntent"]["slots"]["city"]
     complete_url = base_url + "appid=" + api_key + "&q=" + city_name
     response = requests.get(complete_url)
-    x = response.json()
-    content = ""
+    resp_json = response.json()
 
-    if x["cod"] != "404":
-         y = x["main"]
-         current_temperature = y["temp"] - 273.15
-         current_pressure = y["pressure"]
-         current_humidiy = y["humidity"]
-         z = x["weather"]
-         weather_description = z[0]["description"]
-         content = "Temperature (in celsius) = " + str("%.2f" % current_temperature) + "\nAtmospheric pressure (in hPa) = " + str(
-           current_pressure) + "\nHumidity (in percentage) = " + str(current_humidiy) + "\nDescription = " + str(weather_description)
+    if resp_json["cod"] != "404":
+        main_value = resp_json["main"]
+        
+        # temperature in kelvin value, changed to celsius
+        current_temperature = main_value["temp"] - 273.15 
+        
+        current_pressure = main_value["pressure"]
+        current_humidiy = main_value["humidity"]
+        
+        weather_value = resp_json["weather"] 
+        weather_description = weather_value[0]["description"]
+        content = f"""
+            Temperature(in celsius) = {str("%.2f" % current_temperature)}
+            Athmospheric Pressure (in hPa) = {str(current_pressure)}
+            Humidity(in percentage) = {str(current_humidiy)}\n
+            Description = {str(weather_description)}
+            """
     else:
         content = "City not found"
 
